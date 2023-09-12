1d43af4d30fde77c6a9ebd8a4f483e8a31b0f572
Dennis de Oliveira Py <dennispy14@hotmail.com>
2023-09-12 18:22:37 -0300

    Incluido geolocalização, validações de entrada e incluido links de cidades

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
-
-
-const apiKey =  process.env.WEATHER_API_KEY; //Key da Api que retorna o clima OpwnWeather
-const accessKeyImg = process.env.UNSPLASH_API_KEY; //Key API que retorna imagem UnSplash
+const apiKey = "a844eeb5c98602b46ec096912190ad6f"; //Key da Api que retorna o clima OpwnWeather
+const accessKeyImg = "vlFv2yabnopOF2qAL5ZUBYyb8ztSpDZm3ts0Wdl2T9U"; //Key API que retorna imagem UnSplash
+const apiKeyGeocode = '7561cc976aa04c0ab3f0c420a0b20c63'; //Key API que retorna cidade Geocode
 
 const apiCountryURL = "https://flagsapi.com/"; //Api que retorna o icone da bandeira FlagsApi
 const apiCountryURLEnd = "/flat/32.png";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
-
+const cityshome = document.querySelector("#citys");
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
@@ -16,83 +15,129 @@ const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
 const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
-
+const long = document.getElementById("long");
+const lati = document.getElementById("lati");
+const citys = document.getElementById("citys");
 const weatherContainer = document.querySelector("#weather-data");
 
 //Funções
+
+function showLocalization(pos) {
+  buscarCidadePais(pos.coords.longitude, pos.coords.latitude);
+};
+
+function errorLocalization() {
+  console.log("Erro ao obter a localização!");
+}
+
+function buscarCidadePais(longitude, latitude) {
+
+  fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKeyGeocode}`)
+    .then(response => response.json())
+    .then(data => {
+      const resultado = data.results[0];
+      console.log(resultado);
+      if (resultado) {
+        const cidade = resultado.components.city || resultado.components.town || resultado.components.village;
+        showWeatherData(cidade);
+
+      } else {
+        alert('Localização não encontrada.');
+      }
+    })
+    .catch(error => {
+      console.error('Erro ao buscar localização:', error);
+    });
+}
+
 const getWeatherData = async (city) => {
 
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`; //Api que retorna o clima OpwnWeather
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`; //Api que retorna o clima OpwnWeather
 
-    const res = await fetch(apiWeatherURL);
-    const data = await res.json();
-    console.log(data);
-    return data;
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  return data;
 }
 
 const showWeatherData = async (city) => {
-   const data =  await getWeatherData(city);
+  const data = await getWeatherData(city);
 
-   cityElement.innerText = data.name;
-   tempElement.innerText = parseInt(data.main.temp);
-   descElement.innerText = data.weather[0].description;
-   weatherIconElement.setAttribute(
-    "src", 
-    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  if (data.cod != 404) {
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
     );
     countryElement.setAttribute(
-        "src", 
-        apiCountryURL + data.sys.country + apiCountryURLEnd
+      "src",
+      apiCountryURL + data.sys.country + apiCountryURLEnd
     );
     humidityElement.innerText = `${data.main.humidity}%`;
     windElement.innerText = `${data.wind.speed}km/h`;
 
-    weatherContainer.classList.remove("hide");
+    weatherContainer.classList.remove("hide");//Remove a div 
 
-    let el = document.getElementById('body');   
+    citys.style.display = "none"; //Oculta a div
 
+    let el = document.getElementById('body');
 
-   const cidade = data.name;   
-   const urlApiImg = `https://api.unsplash.com/photos/random?query=${cidade}&client_id=${accessKeyImg}`;// API que retorna imagem UnSplash
+    const cidade = data.name;
+    const urlApiImg = `https://api.unsplash.com/photos/random?query=${cidade}&client_id=${accessKeyImg}`;// API que retorna imagem UnSplash
 
     // Requisição GET para a API do Unsplash
     fetch(urlApiImg)
       .then(response => response.json())
       .then(data => {
-        // URL da imagem retornada pela API
+
         const imagemUrl = data.urls.regular;
-    
-        // Elemento <img> para exibir a imagem
         const imagemCidade = document.createElement('img');
-    
-        // Atributo 'src' da imagem com a URL da imagem do Unsplash
         imagemCidade.src = imagemUrl;
-    
-        // Imagem ao seu documento HTML
-        //document.body.appendChild(imagemCidade);
         document.body.style.backgroundImage = `url(${imagemUrl})`;
-        document.body.style.backgroundRepeat = "none"; 
+        document.body.style.backgroundRepeat = "none";
+
       })
       .catch(error => {
         console.log('Ocorreu um erro:', error);
       });
+  } else
+    alert("Digite uma cidade valida!")
 }
 
 // Eventos
-searchBtn.addEventListener("click",(e)=>{
+searchBtn.addEventListener("click", (e) => {
 
-    e.preventDefault();
-
-    const city = cityInput.value;
-
+  e.preventDefault();
+  const city = cityInput.value;
+  if (city)
     showWeatherData(city);
-})
-
-cityInput.addEventListener("keyup", e =>{
-
-    if(e.code === "Enter"){
-        const city = e.target.value;
-        showWeatherData(city);
-    }
+  else
+    alert("Digite uma cidade!");
 
 });
+
+cityshome.addEventListener("click", (e) => {
+
+  e.preventDefault();
+  const city = e.target.textContent;
+  showWeatherData(city);
+
+});
+
+cityInput.addEventListener("keyup", e => {
+
+  if (e.code === "Enter") {
+    const city = e.target.value;
+    showWeatherData(city);
+  }
+
+});
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showLocalization, errorLocalization);
+
+} else {
+  console.log("Geolocalização não suportada");
+};
--- readme.md
+++ readme.md
@@ -1,45 +1,31 @@
 <h1>Weather Finder</h1>
 <p>O Weather Finder é um site que permite aos usuários verificar as condições climáticas de qualquer cidade. Basta digitar o nome da cidade desejada e o site irá fornecer informações precisas e atualizadas sobre o clima.</p>
 
-##<h2>Funcionalidades</h2>
+<h2>Funcionalidades</h2>
 
 + Pesquisa por cidade: Os usuários podem digitar o nome de qualquer cidade para obter informações sobre o clima atual.
 Exibição de informações detalhadas: Além de mostrar a temperatura atual, o site também fornece dados como umidade, velocidade do vento e descrição das condições climáticas.
 + Imagens relacionadas: Para tornar a experiência mais agradável, o site exibe uma imagem representativa da cidade pesquisada, obtida através da API Unsplash.
 + Bandeiras de países: Além das informações climáticas, o site também exibe a bandeira do país correspondente à cidade pesquisada, utilizando a API FlagsApi.
++ Geolocalização: Informa o clima da cidade em que o usuário está localizado.
+
   
-##<h3>APIs utilizadas</h3>
+## <h3>APIs utilizadas</h3>
 + Unsplash API: A API do Unsplash é utilizada para obter imagens relacionadas à cidade pesquisada. Cada imagem exibida é escolhida aleatoriamente entre uma seleção de fotos disponíveis.
 + Weather API: A Weather API é responsável por fornecer os dados meteorológicos atualizados para a cidade pesquisada. Esses dados incluem temperatura, umidade, velocidade do vento, condições climáticas e muito mais.
 + FlagsApi: A FlagsApi é usada para obter a bandeira do país correspondente à cidade pesquisada. A bandeira é exibida juntamente com as informações climáticas para fornecer uma identificação visual rápida.
++ GeoCode: A GeoCode é usada para obter a cidade correspondente do usuário, através da latitude e longetude obtidas pelo navegador.
   
 <h4>Como executar o projeto localmente:</h4>
 
 ## Pré-requisitos
 
-- Node.js (https://nodejs.org) instalado em sua máquina.
-- Chaves de API para acessar as APIs Unsplash e Weather.
+- Chaves de API para acessar as APIs Unsplash, Weather e Geocode.
+
 ## Configuração das chaves de API
 
 1. Clone este repositório em sua máquina local.
-2. Crie um arquivo chamado `.env` na raiz do projeto.
-3. Abra o arquivo `.env` e insira suas chaves de API no seguinte formato:
-  UNSPLASH_API_KEY=SuaChaveUnsplashAqui
-  WEATHER_API_KEY=SuaChaveWeatherAqui
-  <p><i>Substitua "SuaChaveUnsplashAqui", "SuaChaveWeatherAqui" e "SuaChaveFlagsApiAqui" pelas suas respectivas chaves obtidas ao se registrar nas APIs correspondentes.</i><p>
 
-4. Salve o arquivo `.env`.
+2. Substitua ChaveUnsplash, ChaveWeather e ChaveGeocodeApi pelas suas respectivas chaves obtidas ao se registrar nas APIs correspondentes.
 
-Observação: Certifique-se de não compartilhar suas chaves de API publicamente ao fazer commit ou ao compartilhar seu código.
-
-## Como executar o projeto localmente
-
-1. Após configurar corretamente suas chaves de API no arquivo `.env`, abra o terminal na pasta raiz do projeto.
-2. Execute o seguinte comando para instalar as dependências necessárias:
-   npm install
-3. Em seguida, execute o seguinte comando para iniciar o servidor de desenvolvimento com o Webpack:
-   npm start
-4. O servidor será iniciado, e você poderá acessar o projeto em seu navegador.
-5. Digite o nome da cidade desejada na caixa de pesquisa e pressione Enter. Aguarde alguns segundos enquanto o site busca as informações do clima e exibe a imagem e a bandeira correspondentes.
-
-Observação: Se encontrar algum problema ao configurar suas chaves de API ou ao executar o projeto, verifique se o Node.js está instalado corretamente em sua máquina e se as dependências foram instaladas com sucesso usando o comando "npm install".
+4. Salve o arquivo.
